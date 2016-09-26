c527ff98bc772a4d04ab8b9cfd1e36564e9be143
renniegov <renniegov@worldonline.co.za>
2016-09-26 22:20:27 +0200

    Initial Commit

---
+++ MySocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySocial
+//
+//  Created by Rennie on 2016/09/26.
+//  Copyright © 2016 Rennie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
